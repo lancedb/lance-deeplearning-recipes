77dbc155150ca717684ace8c67a6a65edb5bf34a
Vipul Maheshwari <vim.code.level@gmail.com>
2024-04-17 10:39:36 +0530

    all the changes are done, script ready to use

--- converters/lance-image-dataset-converter/README.md
+++ converters/lance-image-dataset-converter/README.md
@@ -1,7 +1,7 @@
 ## Convert any Image dataset to Lance
 
 ### Overview
-The `convert-any-image-dataset-to-lance.py` script serves as a versatile tool for transforming any Image Dataset into the Lance format, enabling seamless integration and analysis. It provides a straightforward solution for converting diverse image datasets into a standardized format for enhanced compatibility and ease of use.
+The `convert-any-image-dataset-to-lance.py` script can be used for transforming any Image Dataset into the Lance format. It provides a straightforward solution for converting diverse image datasets into a standardized Lance format. 
 
 To effortlessly convert your image dataset into Lance format, simply execute the following command in your terminal:
 
@@ -9,7 +9,7 @@ To effortlessly convert your image dataset into Lance format, simply execute the
 python convert-any-image-dataset-to-lance.py --dataset /path/to/your/image_dataset_folder
 ```
 
-This command will seamlessly generate separate Lance files for your training, testing, and validation data. You have the flexibility to tailor the schema and specify which subsets (training, testing, validation) should be included in the Lance files. Even if you possess only one subset (e.g., only training data), you can conveniently indicate which subset(s) you wish to incorporate.
+This command will seamlessly generate separate Lance files for your training, testing, and validation data. You have the flexibility to change the schema and specify which subsets (training, testing, validation) should be included in the Lance files. Even if you have only one subset (e.g., only training data), you can conveniently indicate which subset(s) you wish to incorporate by changing the variables. 
 
 For starters and effortless access to pre-formatted CINIC-10 and mini-ImageNet datasets in Lance format, you can refer to the following Lance Image Dataset links:
 
@@ -18,7 +18,7 @@ CINIC-10 Dataset: https://www.kaggle.com/datasets/vipulmaheshwarii/cinic-10-lanc
 mini-ImageNet Dataset: https://www.kaggle.com/datasets/vipulmaheshwarii/mini-imagenet-lance-dataset
 
 ### Code and Blog
-If you really want to understand the gist of how we are converting an Image Dataset to it's Lance format, refer to the following blog  and referenced colab explaining each step in detail. 
+If you want to understand the process of converting an image dataset to Lance format, please refer to the following blog and it's corresponding Google Colab notebook.
 
 <a href="https://colab.research.google.com/drive/12RjdHmp6m9_Lx7YMRiat4_fYWZ2g63gx?usp=sharing"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"></a> [![Ghost](https://img.shields.io/badge/ghost-000?style=for-the-badge&logo=ghost&logoColor=%23F7DF1E)](https://blog.lancedb.com/convert-any-image-dataset-to-lance/)
 
--- converters/lance-image-dataset-converter/convert-any-image-dataset-to-lance.py
+++ converters/lance-image-dataset-converter/convert-any-image-dataset-to-lance.py
@@ -6,22 +6,17 @@ import lance
 import time
 from tqdm import tqdm
 
-def process_images(images_folder, data_type):
-    # Define schema for RecordBatch
-    schema = pa.schema([('image', pa.binary()), 
-                        ('filename', pa.string()), 
-                        ('category', pa.string()), 
-                        ('data_type', pa.string())])
+def process_images(images_folder, split, schema):
 
     # Iterate over the categories within each data type
-    category_folder = os.path.join(images_folder, data_type)
-    for category in os.listdir(category_folder):
-        category_folder = os.path.join(images_folder, data_type, category)
+    label_folder = os.path.join(images_folder, split)
+    for label in os.listdir(label_folder):
+        label_folder = os.path.join(images_folder, split, label)
         
-        # Iterate over the images within each category
-        for filename in tqdm(os.listdir(category_folder), desc=f"Processing {data_type} - {category}"):
+        # Iterate over the images within each label
+        for filename in tqdm(os.listdir(label_folder), desc=f"Processing {split} - {label}"):
             # Construct the full path to the image
-            image_path = os.path.join(category_folder, filename)
+            image_path = os.path.join(label_folder, filename)
 
             # Read and convert the image to a binary format
             with open(image_path, 'rb') as f:
@@ -29,46 +24,39 @@ def process_images(images_folder, data_type):
 
             image_array = pa.array([binary_data], type=pa.binary())
             filename_array = pa.array([filename], type=pa.string())
-            category_array = pa.array([category], type=pa.string())
-            data_type_array = pa.array([data_type], type=pa.string())
+            label_array = pa.array([label], type=pa.string())
+            split_array = pa.array([split], type=pa.string())
 
             # Yield RecordBatch for each image
             yield pa.RecordBatch.from_arrays(
-                [image_array, filename_array, category_array, data_type_array],
+                [image_array, filename_array, label_array, split_array],
                 schema=schema
             )
 
 # Function to write PyArrow Table to Lance dataset
-def write_to_lance(images_folder):
-    # Create an empty RecordBatchIterator
-    schema = pa.schema([
-        pa.field("image", pa.binary()),
-        pa.field("filename", pa.string()),
-        pa.field("category", pa.string()),
-        pa.field("data_type", pa.string())
-    ])
-
-    for data_type in ['train', 'test', 'val']:
-        lance_file_path = os.path.join(images_folder, f"mini_imagenet_{data_type}.lance")
+def write_to_lance(images_folder, dataset_name, schema):
+    for split in ['train', 'test', 'val']:
+        lance_file_path = os.path.join(images_folder, f"{dataset_name}_{split}.lance")
         
-        reader = pa.RecordBatchReader.from_batches(schema, process_images(images_folder, data_type))
+        reader = pa.RecordBatchReader.from_batches(schema, process_images(images_folder, split, schema))
         lance.write_dataset(
             reader,
             lance_file_path,
             schema,
         )
 
-def loading_into_pandas(images_folder):
+def loading_into_pandas(images_folder, dataset_name):
     data_frames = {}  # Dictionary to store DataFrames for each data type
     
-    for data_type in ['test', 'train', 'val']:
-        uri = os.path.join(images_folder, f"mini_imagenet_{data_type}.lance")
+    batch_size = args.batch_size
+    for split in ['test', 'train', 'val']:
+        uri = os.path.join(images_folder, f"{dataset_name}_{split}.lance")
 
         ds = lance.dataset(uri)
 
         # Accumulate data from batches into a list
         data = []
-        for batch in tqdm(ds.to_batches(columns=["image", "filename", "category", "data_type"], batch_size=10), desc=f"Loading {data_type} batches"):
+        for batch in tqdm(ds.to_batches(columns=["image", "filename", "label", "split"], batch_size=batch_size), desc=f"Loading {split} batches"):
             tbl = batch.to_pandas()
             data.append(tbl)
 
@@ -76,31 +64,49 @@ def loading_into_pandas(images_folder):
         df = pd.concat(data, ignore_index=True)
         
         # Store the DataFrame in the dictionary
-        data_frames[data_type] = df
+        data_frames[split] = df
         
-        print(f"Pandas DataFrame for {data_type} is ready")
+        print(f"Pandas DataFrame for {split} is ready")
         print("Total Rows: ", df.shape[0])
     
     return data_frames
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process image dataset.')
+    parser.add_argument('--batch_size', type=int, default=10, help='Batch size for processing images')
     parser.add_argument('--dataset', type=str, help='Path to the image dataset folder')
-
+    
     try:
         args = parser.parse_args()
         dataset_path = args.dataset
         if dataset_path is None:
             raise ValueError("Please provide the path to the image dataset folder using the --dataset argument.")
+        
+        # Extract dataset name
+        dataset_name = os.path.basename(dataset_path)
 
         start = time.time()
-        write_to_lance(dataset_path)
-        data_frames = loading_into_pandas(dataset_path)
+        schema = pa.schema([
+            pa.field("image", pa.binary()),
+            pa.field("filename", pa.string()),
+            pa.field("label", pa.string()),
+            pa.field("split", pa.string())
+        ])
+        write_to_lance(dataset_path, dataset_name, schema)
+        data_frames = loading_into_pandas(dataset_path, dataset_name)
         end = time.time()
         print(f"Time(sec): {end - start}")
 
     except ValueError as e:
         print(e)
         print("Example:")
-        print("python script_name.py --dataset /path/to/your/image_dataset_folder")
+        print("python3 convert-any-image-dataset-to-lance.py --batch_size 10 --dataset image_dataset_folder")
+        exit(1)
+
+    except FileNotFoundError:
+        print("The provided dataset path does not exist.")
+        exit(1)
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
         exit(1)
